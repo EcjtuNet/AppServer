93ce5bc213d9e3dba59b03c8f960b7a96b9b7707
wtbhk <wtbhk@live.cn>
2015-04-01 17:59:27 +0800

    文件上传fix

--- templates/settings.php
+++ templates/settings.php
@@ -1,7 +1,7 @@
 <?php require 'header.php'; ?>
 <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
   <h2 class="sub-header">设置信息</h2>
-  <form action="" method="POST">
+  <form action="" method="POST" enctype="multipart/form-data">
     <div class="input-group ">
       <span class="input-group-addon" id="sizing-addon1" >版本名</span>
       <input type="text" class="form-control" name="version_name" placeholder="" value="<?php echo Setting::find('version_name')->value;?>" aria-describedby="sizing-addon1">
