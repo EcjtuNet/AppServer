418bf63359aeae428465cf1bb5c93aeee349b1d1
wtbhk <wtbhk@live.cn>
2015-05-21 17:44:15 +0800

    fix

--- public/index.php
+++ public/index.php
@@ -454,8 +454,8 @@ $app->group('/api/v1', function () use ($app) {
 			$sid = $comment->author;
 			$uc = new UserCenter();
 			$user = $uc->getUser($sid);
-			$comment->avatar = $user->avatar;
-			$comment->name = $user->name;
+			$comment->avatar = $user['avatar'];
+			$comment->name = $user['name'];
 			return $comment;
 		});
 		$app->render('api_article_view.php', array(
