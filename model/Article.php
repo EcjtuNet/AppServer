372ac82e03ac57b3881efdb0f31de9e6e9399ca0
wfcsnzdm <530254427@qq.com>
2015-07-27 18:36:47 +0800

    until

--- model/Article.php
+++ model/Article.php
@@ -17,7 +17,7 @@ class Article extends Illuminate\Database\Eloquent\Model {
 	}
 
 	public function scopeUntil ($query, $id) {
-		return $query->where('id','=', $id);
+		return $query->where('id','>', $id);
 	}
 	public function admin() {
 		return $this->belongsTo('Admin');
