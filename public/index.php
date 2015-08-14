534f70a82b9303e6a88623accb1a9c9706a8d16d
wfcsnzdm <530254427@qq.com>
2015-08-14 22:39:59 +0800

    reset

--- public/index.php
+++ public/index.php
@@ -397,10 +397,6 @@ $app->group('/api/v1', function () use ($app) {
 			->take(5)
 			->get();
 		$normal_articles = Article::whereNotIn('id', $image_articles->lists('id'))
-			->whereDoesntHave('categories')
-			->orWhereHas('categories',function ($q){
-				$q->where('id','<>',2)
-			})
 			->newest()
 			->with('categories')
 			->published();
