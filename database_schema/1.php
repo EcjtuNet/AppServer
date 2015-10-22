94700d5817e2c6ffe80868b3f8628b81fccb0e78
wfcsnzdm <530254427@qq.com>
2015-10-22 17:42:48 +0800

    字限制

--- database_schema/1.php
+++ database_schema/1.php
@@ -2,7 +2,7 @@
 use Illuminate\Database\Capsule\Manager as Capsule;
 require '../bootstrap.php';
 
-Capsule::schema()->create('feedbook',function($table){
+Capsule::schema()->create('feedbooks',function($table){
 	$table->increments('id');
 	$table->string('nikename');
 	$table->string('content');
--- database_schema/2.php
+++ database_schema/2.php
@@ -1,4 +1,6 @@
 <?php
 use Illuminate\Database\Capsule\Manager as Capsule;
 require '../bootstrap.php';
-	Capsule::Schema()->rename('feedbook', 'feedbooks');
+	Capsule::Schema()->table('articles',function($table){
+		$table->string('content',2000)->change(60000);
+	});
