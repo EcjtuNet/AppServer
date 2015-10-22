f5e9b9165c7ec5d25b051e7523fec4987a854c1d
wfcsnzdm <530254427@qq.com>
2015-10-22 22:56:17 +0800

    修改字段类型

--- database_schema/2.php
+++ database_schema/2.php
@@ -1,7 +1,10 @@
 <?php
 use Illuminate\Database\Capsule\Manager as Capsule;
 require '../bootstrap.php';
-	Capsule::Schema()->drop('feedbook');
 	Capsule::Schema()->table('articles',function($table){
-		$table->string('content',6000)->change();
+		$table->dropColumn('content');
+	});
+
+	Capsule::Schema()->table('articles',function($table){
+		$table->text('content');
 	});
