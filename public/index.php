e7c15ade1630c7e8c252a3955763defd45acd0fa
wfcsnzdm <530254427@qq.com>
2015-10-24 16:53:23 +0800

    修改反馈接口

--- public/index.php
+++ public/index.php
@@ -460,7 +460,10 @@ $app->group('/api/v1', function () use ($app) {
 			 ));
 		return ;
 	}
-	 $feedbook = new Feedbook;
+	 $feedbook = Feedbook::create(array(
+	 	'nikename'=> $nikename,
+	 	'content' => $content
+	 	));
 	// $feedbook->content = $content;
 	// $feedbook->nikename = $nikename;
 	// $feedbook->save();
