675513916891393341a78ac8d389b198c4325695
wfcsnzdm <530254427@qq.com>
2015-10-24 15:58:59 +0800

    修改反馈接口

--- public/index.php
+++ public/index.php
@@ -461,12 +461,7 @@ $app->group('/api/v1', function () use ($app) {
 		return ;
 	}
 	$feedbook = new Feedbook;
-	$feedbook->content = $content;
-	$feedbook->nikename = $nikename;
-	$feedbook->save();
-	echo json_encode(array(
-		'status' => 200
-		));
+	echo $feedbook;
 	});
 
 	$app->get('/articles', function () use ($app) {
