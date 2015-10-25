f85e5ebbe4474b3b6ae1f40fd38f5c20c13e1c62
wfcsnzdm <530254427@qq.com>
2015-10-25 18:53:31 +0800

    DB_table

--- public/index.php
+++ public/index.php
@@ -460,10 +460,10 @@ $app->group('/api/v1', function () use ($app) {
 			 ));
 		return ;
 	}
-	 $feedbook = Feedbook::create(array(
-	 	'nikename'=> $nikename,
+	 DB::table('feedbooks')->insert([
+	 	'nikename' => $nikename,
 	 	'content' => $content
-	 	));
+	 	]);
 	// $feedbook->content = $content;
 	// $feedbook->nikename = $nikename;
 	// $feedbook->save();
