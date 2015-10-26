b47f16447569fccb7555815a00b5d88e80363bfd
wfcsnzdm <530254427@qq.com>
2015-10-26 19:24:58 +0800

    意见反馈

--- public/index.php
+++ public/index.php
@@ -460,13 +460,10 @@ $app->group('/api/v1', function () use ($app) {
 			 ));
 		return ;
 	}
-	 DB::table('feedbooks')->insert([
-	 	'nikename' => $nikename,
-	 	'content' => $content
-	 	]);
-	// $feedbook->content = $content;
-	// $feedbook->nikename = $nikename;
-	// $feedbook->save();
+	$feedbook = new Feedbook;
+	$feedbook->content = $content;
+	$feedbook->nikename = $nikename;
+	$feedbook->save();
 	echo json_encode(array(
 		'status' => 200
 		));
