a56e3e59925eb7d505241456297a636fa6b6b8e6
wfcsnzdm <530254427@qq.com>
2015-10-28 18:49:11 +0800

    comments

--- public/index.php
+++ public/index.php
@@ -517,7 +517,8 @@ $app->group('/api/v1', function () use ($app) {
 		}
 		$sid = $app->request->params('sid');
 		$token = $app->request->params('token');
-		$user = new UserCenter($sid, $token);
+		$uc = new UserCenter();
+		$user = $uc->getUser($sid,$token);
 		if(!$user){
 			echo json_encode(array('status'=>403));
 			return ;
